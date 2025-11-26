1a46911c86fcd6b871b5eef3e29f4bd676984c52
Shiroi Negai <shiroinegai@proton.me>
2025-11-26 10:55:18 +0800

    add trekker assets

--- src/lib/data/trekkers.ts
+++ src/lib/data/trekkers.ts
@@ -2,4 +2,13 @@ import TREKKERS_JSON from '$SSData/character.json';
 
 const trekkers = TREKKERS_JSON;
 
-export const TREKKERS = Object.entries(trekkers).map((trekker) => trekker[1]);
+export const TREKKERS = Object.entries(trekkers).map((trekker) => ({
+	id: trekker[1].id,
+	name: trekker[1].name,
+	quality: trekker[1].star,
+	element: trekker[1].element,
+	attackType: trekker[1].attackType,
+	position: trekker[1].class,
+	style: trekker[1].tag[1],
+	faction: trekker[1].tag[2]
+}));
--- src/routes/trekkers/+page.svelte
+++ src/routes/trekkers/+page.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
+
 	import { TREKKERS } from '$lib/data/trekkers';
 </script>
 
+<svelte:head>
+	<title>Trekkers • Monolith Wish</title>
+</svelte:head>
+
 <div>
-	{#each TREKKERS as { id, name, star, element, attackType, tag, ...rest }, i (`${i}${id}`)}
-		<p>{id} {name} {star} star</p>
-		<p>
-			{element}, {rest.class}, {attackType}, {tag},
-		</p>
+	{#each TREKKERS as { id, name, quality, element, attackType, position }, i (`${i}${id}`)}
+		<a href={resolve('/trekkers/[id]', { id: id.toString() })}>
+			<p>{name} {quality} star</p>
+			<p>
+				{element}, {position}, {attackType}
+			</p>
+		</a>
 	{/each}
 </div>
 
 <style>
 	div {
+		display: flex;
 		max-height: 100dvb;
 		overflow: auto;
 		scrollbar-width: none;
